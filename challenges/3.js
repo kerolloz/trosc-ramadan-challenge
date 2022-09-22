c301223d5498faf7db229d8968e1f5eaff839544
Haidy Yasser <99363484+Haidyasser@users.noreply.github.com>
2022-09-22 23:39:29 +0200

    Update 3.js
    the bug fixed

--- challenges/3.js
+++ challenges/3.js
@@ -6,7 +6,7 @@
  * @param {number} n
  */
 function isPrime(n) {
-  if (n == 2) return true;
+  if (n < 2) return false;
   for (let i = 2; i < n; i++) if (n % i == 0) return false;
   return true;
 }
